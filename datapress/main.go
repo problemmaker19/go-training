a36e1cc465f2813aad6bae87645d09d504461547
problemmaker19 <iesambaev@gmail.com>
2024-02-09 19:02:10 +0300

    problem

--- datapress/main.go
+++ datapress/main.go
@@ -5,27 +5,48 @@ import (
 	"fmt"
 	"os"
 	"strconv"
+	"strings"
 )
-
-func lineReadInt(s *bufio.Scanner) int {
-	s.Scan()
-	v, _ := strconv.Atoi(s.Text())
-
-	return v
-}
 
 func compress(s *bufio.Scanner) {
 	s.Scan()
-	s.Scan()
-	data := s.Text()
-	fmt.Println(data)
+	text := s.Text()
+
+	arr := strings.Split(text, " ")
+	prev, _ := strconv.Atoi(arr[0])
+	counter := 0
+
+	for i, v := range arr {
+		if i == 0 {
+			continue
+		}
+
+		cur, _ := strconv.Atoi(v)
+
+		if prev-cur == 1 {
+			prev = cur
+			counter--
+			continue
+		}
+		// if cur-prev == 1 {
+		// 	prev = cur
+		// 	continue
+		// }
+
+		counter = 0
+		fmt.Print(-counter-cur, counter, "; ")
+		prev = cur
+	}
+	fmt.Println()
 }
 
 func main() {
 	scanner := bufio.NewScanner(os.Stdin)
-	tasks := lineReadInt(scanner)
+	scanner.Scan()
+	tasks, _ := strconv.Atoi(scanner.Text())
 
 	for i := 0; i < tasks; i++ {
+		scanner.Scan()
 		compress(scanner)
 	}
 }
